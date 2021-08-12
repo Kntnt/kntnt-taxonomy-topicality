0be1a5cb7f03aee093428451ebedf824048a9725
Thomas Barregren <thomas@kntnt.com>
2021-08-12 11:36:08 +0200

    Update kntnt-taxonomy-topicality.php

--- kntnt-taxonomy-topicality.php
+++ kntnt-taxonomy-topicality.php
@@ -6,7 +6,7 @@
  * Plugin Name:       Kntnt Topicality Taxonomy
  * Plugin URI:        https://www.kntnt.com/
  * Description:       Provides the `topicality` taxonomy whose terms indicate the topicality of a post (e.g. `evergreen`, `up to date`, `outdated`, and `obsolete`).
- * Version:           1.0.2
+ * Version:           1.0.3
  * Author:            Thomas Barregren
  * Author URI:        https://www.kntnt.com/
  * License:           GPL-3.0+
@@ -75,7 +75,7 @@ class Taxonomy {
 
 			// Whether to display a column for the taxonomy on its post
 			// type listing screens.
-			'show_admin_column' => true,
+			'show_admin_column' => false,
 
 			// Metabox to show on edit. If a callable, it is called to render
 			// the metabox. If `null` the default metabox is used. If `false`,
